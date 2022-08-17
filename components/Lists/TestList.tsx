33626abfef91c59e46683e662565cf6425813f9e
Marius Devisevici <marius.devisevici@virtualink.ro>
2022-08-17 14:07:26 +0300

    prod test

---
+++ components/Lists/TestList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestList() {
+  return <div>TestList</div>;
+}
+
+export default TestList;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        brutalShadow: "2px 2px 0px rgba(0, 0, 0, 1)",
+      },
+    },
   },
   plugins: [],
 };
